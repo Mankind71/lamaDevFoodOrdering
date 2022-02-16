fc57088c4b682543674e3f36ac2945f81eeae394
Mankind71 <makindetolulope37@gmail.com>
2022-02-16 14:17:19 +0100

    created pizza card and pizza list components... worked on footer

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -1,14 +1,25 @@
-import style from '../styles/PizzaList.module.css'
+import styles from '../styles/PizzaList.module.css'
 import PizzaCard from './PizzaCard';
 
 const PizzaList = () => {
   return (
-    <div className={style.container}>
-      <h1 className={style.title}>THE BEST PIZZA IN TOWN</h1>
-      <p className={style.desc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur atque ea numquam fuga. Deleniti expedita perferendis cupiditate eligendi perspiciatis quia at recusandae enim repellat ab? Necessitatibus, autem? Ea maiores mollitia vero nisi neque enim, voluptate ullam incidunt rem velit explicabo hic, voluptas ipsa delectus eligendi possimus optio recusandae dignissimos?
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        pariatur atque ea numquam fuga. Deleniti expedita perferendis cupiditate
+        eligendi perspiciatis quia at recusandae enim repellat ab?
+        Necessitatibus, autem? Ea maiores mollitia vero nisi neque enim,
+        voluptate ullam incidunt rem velit explicabo hic, voluptas ipsa delectus
+        eligendi possimus optio recusandae dignissimos?
       </p>
-      <div className={style.wrapper}>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
         <PizzaCard />
       </div>
     </div>
