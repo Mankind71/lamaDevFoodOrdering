949660264f1ba40b02ba9c0147d978bb15ae5636
Mankind71 <makindetolulope37@gmail.com>
2022-02-19 15:43:05 +0100

    worked on the product page

---
+++ pages/api/products/[id].js
@@ -0,0 +1,44 @@
+import Product from "../../../models/Product";
+import dbConnect from "../../../util/mongo";
+
+export default async function handler(req, res) {
+  const { method, query:{id} } = req;
+
+  dbConnect();
+
+  if (method === "GET") {
+    try {
+      const product = await Product.findById(id);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  if (method === "POST") {
+    try {
+      const product = await Product.create(req.body);
+      res.status(201).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  if (method === "PUTT") {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  if (method === "DELETE ") {
+    try {
+      const product = await Product.create(req.body);
+      res.status(201).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Featured from '../components/Featured'
-import PizzaList from '../components/PizzaList'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+import axios from "axios";
 
-export default function Home() {
-  
-
+export default function Home({ pizzaList }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +15,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <PizzaList /> 
+      <PizzaList pizzaList={pizzaList} />
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzaList: res.data,
+    },
+  };
+};
